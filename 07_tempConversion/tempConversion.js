901be231037bf73c312a2f9ac1c8bd51a95844a9
alahdili <aymanlahdili25@gmail.com>
2023-05-08 20:09:03 -0400

    Completed Exercice 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInFahr) {
+  return Math.round((tempInFahr - 32) * (5/9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCels) {
+  return Math.round(((tempInCels * (9/5)) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
