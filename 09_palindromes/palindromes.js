ab8bb381dfad3e7fa11046e84c1facfe9480059b
alahdili <aymanlahdili25@gmail.com>
2023-05-13 16:48:08 -0400

    Completed exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const alpha = "abcdefghijklmnopqrstuvwxyz";
 
+    str = str.toLowerCase();
+    let l=0, r=str.length;
+    while (l<r) {
+        if (!(alpha.includes(str[l]))) {l++; continue;}
+        if (!(alpha.includes(str[r]))) {r--; continue;}
+        if (str[l] != str[r]) return false;
+
+        l++;
+        r--;
+    }
+    return true;
 };
 
 // Do not edit below this line
