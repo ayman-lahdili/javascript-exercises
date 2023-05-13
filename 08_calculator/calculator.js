d803606fef1c64f9b4c427c166623b283950563b
alahdili <aymanlahdili25@gmail.com>
2023-05-13 16:36:30 -0400

    Completed exercice 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,28 @@
-const add = function() {
-	
+const add = function(x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+  return x-y;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((total, num) => total + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.length
+  ? array.reduce((total, num) => total * num)
+  : 0;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+  return Math.pow(x, y);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num<=1) return 1;
+	return num * factorial(num-1);
 };
 
 // Do not edit below this line
